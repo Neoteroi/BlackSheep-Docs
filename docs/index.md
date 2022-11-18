52fbb8c2fa6fcfd5a9fb951262508e75bca7dcff
Roberto Prevato <roberto.prevato@gmail.com>
2022-11-18 23:38:14 +0100

    General maintenance

--- docs/index.md
+++ docs/index.md
@@ -10,17 +10,18 @@ pip install blacksheep
 ```
 
 ## BlackSheep offers...
+
 * A rich code API, based on dependency injection and inspired by Flask and
   ASP.NET Core
 * A typing-friendly codebase, which enables a comfortable development
   experience thanks to hints when coding with IDEs
-* Built-in generation of OpenAPI Documentation, supporting version 3, YAML and
+* Built-in generation of OpenAPI Documentation, supporting version 3, YAML, and
   JSON
 * A cross platform framework, using the most modern versions of Python
-* [Good performance, as demonstrated by the results from TechEmpower
-  benchmarks](https://github.com/RobertoPrevato/BlackSheep/wiki/Server-performance)
+* Good performance
 
 ## Getting started
+
 To get started with BlackSheep, read these tutorials:
 
 * [Basics](./getting-started/)
--- fixlinks.sh
+++ fixlinks.sh
@@ -1,45 +1,49 @@
 echo "Fixing links..."
 
+if [ -n "$VERSION" ]; then
+  VERSION="/$VERSION"
+fi
+
 for i in ./site/*.html
 do
   echo "Fixing $i"
-  sed -i -E 's,href="\.\.?",href="/blacksheep/",' $i
-  sed -i -E 's,base: ".",base: "/blacksheep/",' $i
-  sed -i -E 's,worker: "assets/,worker: "/blacksheep/assets/,' $i
-  sed -i -E 's,src="\.\/([a-z]+),src="/blacksheep/\1,' $i
-  sed -i -E 's,href="\.\/([a-z]+),href="/blacksheep/\1,' $i
-  sed -i -E 's,src="([a-z]+),src="/blacksheep/\1,' $i
-  sed -i -E 's,href="([a-z]+),href="/blacksheep/\1,' $i
-  sed -i -E 's,src="([a-z]+),src="/blacksheep/\1,' $i
-  sed -i -E 's,src="img/,src="/blacksheep/img/,' $i
-  sed -i -E 's,src="../,src="/blacksheep/,' $i
-  sed -i -E 's,href="../,href="/blacksheep/,' $i
-  sed -i -E 's,href="css/,href="/blacksheep/css/,' $i
-  sed -i -E 's,href="img/,href="/blacksheep/img/,' $i
-  sed -i -E 's,href="assets/,href="/blacksheep/assets/,' $i
+  sed -i -E 's,href="\.\.?",href="/blacksheep'$VERSION'/",' $i
+  sed -i -E 's,base: ".",base: "/blacksheep'$VERSION'/",' $i
+  sed -i -E 's,worker: "assets/,worker: "/blacksheep'$VERSION'/assets/,' $i
+  sed -i -E 's,src="\.\/([a-z]+),src="/blacksheep'$VERSION'/\1,' $i
+  sed -i -E 's,href="\.\/([a-z]+),href="/blacksheep'$VERSION'/\1,' $i
+  sed -i -E 's,src="([a-z]+),src="/blacksheep'$VERSION'/\1,' $i
+  sed -i -E 's,href="([a-z]+),href="/blacksheep'$VERSION'/\1,' $i
+  sed -i -E 's,src="([a-z]+),src="/blacksheep'$VERSION'/\1,' $i
+  sed -i -E 's,src="img/,src="/blacksheep'$VERSION'/img/,' $i
+  sed -i -E 's,src="../,src="/blacksheep'$VERSION'/,' $i
+  sed -i -E 's,href="../,href="/blacksheep'$VERSION'/,' $i
+  sed -i -E 's,href="css/,href="/blacksheep'$VERSION'/css/,' $i
+  sed -i -E 's,href="img/,href="/blacksheep'$VERSION'/img/,' $i
+  sed -i -E 's,href="assets/,href="/blacksheep'$VERSION'/assets/,' $i
   sed -i -E 's,/blacksheep/https://,https://,' $i
-  sed -i -E 's,src="/img,src="/blacksheep/img,' $i
+  sed -i -E 's,src="/img,src="/blacksheep'$VERSION'/img,' $i
 done
 
 
 for i in ./site/**/*.html
 do
   echo "Fixing $i"
-  sed -i -E 's,href="\.\.?",href="/blacksheep/",' $i
-  sed -i -E 's,base: "\.\.",base: "/blacksheep/",' $i
-  sed -i -E 's,worker: "\.\./assets/,worker: "/blacksheep/assets/,' $i
-  sed -i -E 's,src="\.\/([a-z]+),src="/blacksheep/\1,' $i
-  sed -i -E 's,href="\.\/([a-z]+),href="/blacksheep/\1,' $i
-  sed -i -E 's,src="([a-z]+),src="/blacksheep/\1,' $i
-  sed -i -E 's,href="([a-z]+),href="/blacksheep/\1,' $i
-  sed -i -E 's,src="([a-z]+),src="/blacksheep/\1,' $i
-  sed -i -E 's,src="img/,src="/blacksheep/img/,' $i
-  sed -i -E 's,src="../,src="/blacksheep/,' $i
-  sed -i -E 's,href="../,href="/blacksheep/,' $i
-  sed -i -E 's,href="css/,href="/blacksheep/css/,' $i
-  sed -i -E 's,href="img/,href="/blacksheep/img/,' $i
-  sed -i -E 's,href="assets/,href="/blacksheep/assets/,' $i
-  sed -i -E 's,src="/img,src="/blacksheep/img,' $i
+  sed -i -E 's,href="\.\.?",href="/blacksheep'$VERSION'/",' $i
+  sed -i -E 's,base: "\.\.",base: "/blacksheep'$VERSION'/",' $i
+  sed -i -E 's,worker: "\.\./assets/,worker: "/blacksheep'$VERSION'/assets/,' $i
+  sed -i -E 's,src="\.\/([a-z]+),src="/blacksheep'$VERSION'/\1,' $i
+  sed -i -E 's,href="\.\/([a-z]+),href="/blacksheep'$VERSION'/\1,' $i
+  sed -i -E 's,src="([a-z]+),src="/blacksheep'$VERSION'/\1,' $i
+  sed -i -E 's,href="([a-z]+),href="/blacksheep'$VERSION'/\1,' $i
+  sed -i -E 's,src="([a-z]+),src="/blacksheep'$VERSION'/\1,' $i
+  sed -i -E 's,src="img/,src="/blacksheep'$VERSION'/img/,' $i
+  sed -i -E 's,src="../,src="/blacksheep'$VERSION'/,' $i
+  sed -i -E 's,href="../,href="/blacksheep'$VERSION'/,' $i
+  sed -i -E 's,href="css/,href="/blacksheep'$VERSION'/css/,' $i
+  sed -i -E 's,href="img/,href="/blacksheep'$VERSION'/img/,' $i
+  sed -i -E 's,href="assets/,href="/blacksheep'$VERSION'/assets/,' $i
+  sed -i -E 's,src="/img,src="/blacksheep'$VERSION'/img,' $i
   sed -i -E 's,/blacksheep/https://,https://,' $i
 done
 
